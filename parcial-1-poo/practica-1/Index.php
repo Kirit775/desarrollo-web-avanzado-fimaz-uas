3d7f4c7fd5b22291dc2819787e2b6ea8a30c9c0a
Maciel_gnz <116605027+Kirit775@users.noreply.github.com>
2026-03-08 11:16:09 -0700

    Agregar pruebas de la clase Usuario

---
+++ parcial-1-poo/practica-1/Index.php
@@ -0,0 +1,16 @@
+<?php
+        // Incluir el archivo de la clase Usuario
+        require_once 'Usuario.php';
+        
+        // Crear una instancia de Usuario
+        $usuario = new Usuario("Maciel Gonzalez", "macielalain@gmail.com");
+        
+
+        echo "<h1>Informacion del usuario</h1>";
+        // Mostrar los valores usando los getters
+        echo "<div class='info'>";
+        echo "<strong>Nombre:</strong> " . $usuario->getNombre() . "<br>";
+        echo "<strong>Correo:</strong> " . $usuario->getCorreo();
+        echo "</div>";
+        
+?>
